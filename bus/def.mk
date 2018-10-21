e536d4990b9fe11ecba53454e14c7ece4f474e92
Kevin Guran <kevin@gruzzlymug.com>
2018-10-21 06:16:40 +0000

    Add basic server

--- bus/def.mk
+++ bus/def.mk
@@ -5,8 +5,8 @@ type := bin
 # Third party paths
 # include_path_list := /ext/webm/include
 include_path_list := ./include
-# lib_path_list := /ext/webm/something
-
+lib_path_list := /usr/local/lib
+source_lib_list := zmq
 
 # Source files to be built
 source_list := main.c
