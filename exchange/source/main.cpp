0a6c26e4126d8eea27c411a0d9a153668951a2b7
Kevin Guran <kevin@gruzzlymug.com>
2018-10-22 05:17:31 +0000

    Add listener thread to exchange

--- exchange/source/main.cpp
+++ exchange/source/main.cpp
@@ -1,9 +1,13 @@
 #include "order_book.h"
 
-#include <zmq.h>
+#include "zmq.h"
+#include "zhelpers.h"
 
+#include <cassert>
 #include <iostream>
 #include <thread>
+
+#include <cstring>
 
 using namespace finc;
 using namespace std;
@@ -16,10 +20,39 @@ private:
     bool side_;
 };
 
-void listen()
+void listen(void *context)
 {
-    cout << "Connecting to bus…\n";
-    void *context = zmq_ctx_new();
+    // Socket to talk to clients
+    void *subscriber = zmq_socket(context, ZMQ_SUB);
+    int rc = zmq_connect(subscriber, "tcp://localhost:5556");
+    assert(rc == 0);
+
+    char *filter = "94965";
+    rc = zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, filter, strlen(filter));
+    assert(rc == 0);
+
+    int seq_num = 0;
+    long total_temp = 0;
+    while (true) {
+        char *string = s_recv(subscriber);
+        cout << "Received message " << seq_num << "\n";
+
+        int zipcode, temperature, rel_humidity;
+        sscanf(string, "%d %d %d", &zipcode, &temperature, &rel_humidity);
+        total_temp += temperature;
+        free(string);
+
+        ++seq_num;
+        if (seq_num > 100) break;
+    }
+    cout << "Average temperature for zipcode " << filter
+         << " was " << static_cast<int>(total_temp / seq_num) << endl;
+    zmq_close(subscriber);
+    zmq_ctx_destroy(context);
+}
+
+void send(void *context)
+{
     void *requester = zmq_socket(context, ZMQ_REQ);
     zmq_connect(requester, "tcp://localhost:5555");
 
@@ -34,17 +67,18 @@ void listen()
         if (seq_num > 8) break;
     }
     zmq_close(requester);
-    zmq_ctx_destroy(context);
 }
 
 int main()
 {
-    cout << "Order Book Test ][\n";
+    cout << "Starting Exchange\n";
 
     order_book<order> book;
     book.add_order();
 
-    thread listener(listen);
-
+    cout << "Connecting to bus\n";
+    void *context = zmq_ctx_new();
+    thread listener(listen, context);
     listener.join();
+    zmq_ctx_destroy(context);
 }
